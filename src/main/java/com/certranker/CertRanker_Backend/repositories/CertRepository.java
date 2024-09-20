3c91638696c818d10b5582c4a47564b031ad8220
Adam <awhiles2011@gmail.com>
2024-09-20 16:43:58 -0500

    Added Cert Repository

---
+++ src/main/java/com/certranker/CertRanker_Backend/repositories/CertRepository.java
@@ -0,0 +1,18 @@
+package com.certranker.CertRanker_Backend.repositories;
+
+import com.azure.spring.data.cosmos.repository.CosmosRepository;
+import com.certranker.CertRanker_Backend.entities.Cert;
+import jakarta.validation.constraints.NotNull;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+
+@Repository
+public interface CertRepository extends CrudRepository<Cert, String> {
+    @NotNull
+    List<Cert> findAll();
+    Cert findOneById(String id);
+
+}
