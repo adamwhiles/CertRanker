fa833f15618f91403c98d6f30c9390084d118c30
Adam <awhiles2011@gmail.com>
2024-09-20 16:42:48 -0500

    Added CosmosDB Configuration

---
+++ src/main/java/com/certranker/CertRanker_Backend/config/AzureCosmosDbConfiguration.java
@@ -0,0 +1,69 @@
+package com.certranker.CertRanker_Backend.config;
+
+import com.azure.core.credential.AzureKeyCredential;
+import com.azure.cosmos.CosmosClientBuilder;
+import com.azure.cosmos.DirectConnectionConfig;
+import com.azure.cosmos.GatewayConnectionConfig;
+import com.azure.spring.cloud.core.service.AzureServiceType;
+import com.azure.spring.data.cosmos.config.AbstractCosmosConfiguration;
+import com.azure.spring.data.cosmos.config.CosmosConfig;
+import com.azure.spring.data.cosmos.core.ResponseDiagnostics;
+import com.azure.spring.data.cosmos.core.ResponseDiagnosticsProcessor;
+import com.azure.spring.data.cosmos.repository.config.EnableCosmosRepositories;
+import jakarta.annotation.Nullable;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableCosmosRepositories(basePackages = "com.certranker.CertRanker_Backend.repositories")
+public class AzureCosmosDbConfiguration extends AbstractCosmosConfiguration {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(AzureServiceType.AppConfiguration.class);
+
+    @Value("${azure.cosmos.uri}")
+    private String uri;
+
+    @Value("${azure.cosmos.key}")
+    private String key;
+
+    @Value("${azure.cosmos.database}")
+    private String dbName;
+
+    private AzureKeyCredential azureKeyCredential;
+
+    @Bean
+    public CosmosClientBuilder getCosmosClientBuilder() {
+        this.azureKeyCredential = new AzureKeyCredential(key);
+        DirectConnectionConfig directConnectionConfig = DirectConnectionConfig.getDefaultConfig();
+        GatewayConnectionConfig gatewayConnectionConfig = new GatewayConnectionConfig();
+
+        return new CosmosClientBuilder()
+                .endpoint(uri)
+                .credential(azureKeyCredential)
+                .directMode(directConnectionConfig);
+
+    }
+
+    @Bean
+    public CosmosConfig cosmosConfig() {
+        return CosmosConfig.builder()
+                .responseDiagnosticsProcessor(new ResponseDiagnosticsProcessorImplementation())
+                .enableQueryMetrics(true)
+                .build();
+    }
+
+    private static class ResponseDiagnosticsProcessorImplementation implements ResponseDiagnosticsProcessor {
+        @Override
+        public void processResponseDiagnostics(@Nullable ResponseDiagnostics responseDiagnostics) {
+            LOGGER.info("Response Diagnostics {}", responseDiagnostics);
+        }
+    }
+
+    @Override
+    protected String getDatabaseName() {
+        return dbName;
+    }
+}
