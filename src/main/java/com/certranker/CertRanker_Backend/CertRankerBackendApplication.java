9d930d265f8982f9e979c8662e8d7e4c7af08996
Adam <awhiles2011@gmail.com>
2024-09-20 11:56:28 -0500

    Initial commit

---
+++ src/main/java/com/certranker/CertRanker_Backend/CertRankerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.certranker.CertRanker_Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CertRankerBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CertRankerBackendApplication.class, args);
+	}
+
+}
