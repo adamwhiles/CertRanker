bbad41d1d1301fee4b2376a9eac2c306c7037546
Adam <awhiles2011@gmail.com>
2024-09-21 08:25:05 -0500

    Added single cert page and route

--- src/main/java/com/certranker/CertRanker_Backend/controllers/IndexController.java
+++ src/main/java/com/certranker/CertRanker_Backend/controllers/IndexController.java
@@ -3,6 +3,7 @@ package com.certranker.CertRanker_Backend.controllers;
 import com.certranker.CertRanker_Backend.services.CertService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -18,5 +19,11 @@ public class IndexController {
     public String index(Model model) {
         model.addAttribute("certs",certService.findAll());
         return "index";
+    }
+
+    @RequestMapping("/certs/{certId}")
+    public String showCert(@PathVariable String certId, Model model) {
+        model.addAttribute("cert", certService.findOneById(certId));
+        return "cert";
     }
 }
