42cb8d1a228ec384d30b4ca1488481a56ba6e7f4
Adam <awhiles2011@gmail.com>
2024-09-21 07:32:45 -0500

    Updated homepage to show actual cert information instead of dummy information

--- src/main/java/com/certranker/CertRanker_Backend/controllers/IndexController.java
+++ src/main/java/com/certranker/CertRanker_Backend/controllers/IndexController.java
@@ -1,13 +1,22 @@
 package com.certranker.CertRanker_Backend.controllers;
 
+import com.certranker.CertRanker_Backend.services.CertService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class IndexController {
 
+    private final CertService certService;
+
+    public IndexController(CertService certService) {
+        this.certService = certService;
+    }
+
     @RequestMapping("/")
-    public String index() {
+    public String index(Model model) {
+        model.addAttribute("certs",certService.findAll());
         return "index";
     }
 }
