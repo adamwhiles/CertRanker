914459069ef5d9a6ac3daa994037277d0f224be3
Adam <awhiles2011@gmail.com>
2024-09-24 14:11:22 -0500

    form field styling and validation improvements for login and register

--- src/main/java/com/certranker/CertRankerBackend/controllers/UserController.java
+++ src/main/java/com/certranker/CertRankerBackend/controllers/UserController.java
@@ -12,6 +12,7 @@ import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.Optional;
+import java.util.regex.Pattern;
 
 @RestController
 public class UserController {
@@ -28,15 +29,33 @@ public class UserController {
 
 
     @PostMapping("/registernewuser")
-    public ModelAndView registerUser(@RequestParam("email") String email, @RequestParam("password") String password, @RequestParam("username") String username, RedirectAttributes redirectAttributes) {
+    public ModelAndView registerUser(@RequestParam("email") String email, @RequestParam("password") String password, @RequestParam("confirm-password") String confirmPassword, @RequestParam("username") String username, RedirectAttributes redirectAttributes) {
+
+        final Pattern SPECIAL_CHAR_PATTERN = Pattern.compile("[!@#$%^&*(),.?\":{}|<>]");
+
 
         ModelAndView modelAndView = new ModelAndView();
         if (userService.userExists(email)) {
             redirectAttributes.addFlashAttribute("error", "User with this email already exists");
             modelAndView.setViewName("redirect:/register");
-
             return modelAndView;
         }
+        if (!password.equals(confirmPassword)) {
+            redirectAttributes.addFlashAttribute("error", "Passwords do not match");
+            modelAndView.setViewName("redirect:/register");
+            return modelAndView;
+        }
+        if (password.length() < 8) {
+            redirectAttributes.addFlashAttribute("error", "Password must be at least 8 characters long.");
+            modelAndView.setViewName("redirect:/register");
+            return modelAndView;
+        }
+        if (!SPECIAL_CHAR_PATTERN.matcher(password).find()) {
+            redirectAttributes.addFlashAttribute("error", "Password must contain at least one special character.");
+            modelAndView.setViewName("redirect:/register");
+            return modelAndView;
+        }
+
 
         User newUser = new User(username, password, email, "EMAIL");
 
